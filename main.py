446c32a6bf41f188ff72baa47a5a11c9bf012a1f
Toke Henrik Olesen <tokeholesen@gmail.com>
2024-07-13 14:53:38 +0200

    Added barcode type recognition, reworked check digit control, full support for both EAN formats and UPC-A

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from typing import Generator
 
+SIDE_GUARD = "101"
+MIDDLE_GUARD = "01010"
+DIGITS_PER_SIDE = {"EAN-13": 6, "UPC": 6, "EAN-8": 4}
+UNITS_PER_SIDE = {"EAN-13": 42, "UPC": 42, "EAN-8": 28}
+
 # Values for encoding the left-hand side of the barcode, in decimal.
 # For EAN-13, the values with key 0 have odd parity (Set A), while those with key 1 have even parity (Set B).
 # For UPC, always use key 0 values.
@@ -16,7 +21,7 @@ left_encoding = {
     9: {0: 11, 1: 23}
 }
 
-# Values for encoding the right-hand side of the barcode, in decimal. For both EAN-13 and UPC.
+# Values for encoding the right-hand side of the barcode, in decimal. For both EAN and UPC.
 right_encoding = {
     0: 114,
     1: 102,
@@ -46,25 +51,45 @@ ean_13_encoding = {
     9: 26
 }
 
-SIDE_GUARD = "101"
-MIDDLE_GUARD = "01010"
-UNITS_PER_SIDE = 42
+
+def main():
+    barcode = "812345678901"
+    barcode_type = get_type(barcode)
+    barcode_string = encode_barcode(barcode, type=barcode_type)
+    generate_pbm_file(barcode_string, unit_width=6, barcode_height=350, notch_height=35, border=50, type=barcode_type)
 
 
 def checksum_is_correct(barcode_number: str) -> bool:
     """Returns True if the checksum number is correct."""
+    # Gets the last digit of the barcode, which is the check digit
     check_digit = int(barcode_number[-1])
-    barcode_number = barcode_number[:-1]
+    # Removes the last digit and reverses the order of the remaining numbers
+    barcode_number = barcode_number[-2::-1]
     checksum = 0
     for i, digit in enumerate(barcode_number):
-        # Note: because of 0-based indexing, we're adding to odd_sum when i is even and vice versa.
         if i % 2 == 0:
-            checksum += int(digit)
-        else:
             checksum += int(digit) * 3
+        else:
+            checksum += int(digit) * 1
     checksum = 0 if (checksum % 10) == 0 else 10 - (checksum % 10)
     return check_digit == checksum
 
+
+def get_type(barcode: str) -> str:
+    """If the number is a valid barcode number, returns its type."""
+    if not barcode.isnumeric():
+        raise ValueError("Barcode number must be numeric.")
+    number_of_digits = len(barcode)
+    match number_of_digits:
+        case 13:
+            return "EAN-13"
+        case 12:
+            return "UPC"
+        case 8:
+            return "EAN-8"
+        case _:
+            raise ValueError("Incorrect number of digits.")
+        
 
 def get_bits(number: int, length: int) -> Generator[bool, None, None]:
     """Generates the specified number of bits of a number, starting with the least significant bit."""
@@ -79,13 +104,10 @@ def encode_digit(digit: int, parity=None) -> str:
     If no "parity" keyword argument is provided, uses right-hand encoding.
     """
     value = left_encoding[digit][parity] if parity is not None else right_encoding[digit]
-    bit_string = ""
-    for bit in get_bits(value, 7):
-        bit_string += str(bit)
-    return bit_string
+    return "".join(str(bit) for bit in get_bits(value, 7))
 
 
-def encode_left_side(barcode_number: str) -> str:
+def encode_left_side(barcode_number: str, type: str) -> str:
     """
     Encodes the left-hand side of the barcode (the first 7 digits) and returns a string of bits.
     The first digit is encoded as a combination of left and right parity values of the other 6 digits.
@@ -94,54 +116,62 @@ def encode_left_side(barcode_number: str) -> str:
     Then uses the bit as the key to get the correct value from left_encoding.
     """
     leading_digit = int(barcode_number[0])
-    left_digits = barcode_number[1:7]
+    left_digits = barcode_number[1:(DIGITS_PER_SIDE[type] + 1)]
+    
     output = ""
     for i, digit in enumerate(left_digits):
-        # Gets the bit to be used as the key to get the correct value from left_encoding
+        # Gets the bit to be used as the key to get the correct value from left_encoding.
         parity = ean_13_encoding[leading_digit] >> (5 - i) & 1
         output += encode_digit(int(digit), parity)
     return output
 
 
-def encode_right_side(barcode_number: str) -> str:
+def encode_right_side(barcode_number: str, type: str) -> str:
     """Encodes the right-hand side of the barcode (the final 6 digits) and returns a string of bits."""
-    right_digits = barcode_number[7:]
-    output = ""
-    for digit in right_digits:
-        output += encode_digit(int(digit))
-    return output
+    right_digits = barcode_number[(DIGITS_PER_SIDE[type] + 1):]
+    return "".join(encode_digit(int(digit)) for digit in right_digits)
 
 
-def encode_barcode(barcode_number: str) -> str:
+def encode_barcode(barcode_number: str, type: str="EAN-13") -> str:
     """Returns the entire barcode as a string of bits."""
-    left_side = encode_left_side(barcode_number)
-    right_side = encode_right_side(barcode_number)
+    # Adds a leading zero if the barcode is in UPC or EAN-13 format.
+    barcode_number = "0" + barcode_number if type in ["UPC", "EAN-8"] else barcode_number
+    left_side = encode_left_side(barcode_number, type)
+    right_side = encode_right_side(barcode_number, type)
     return f"{SIDE_GUARD}{left_side}{MIDDLE_GUARD}{right_side}{SIDE_GUARD}"
 
 
-def generate_notches(unit_width: int) -> str:
+def generate_notches(unit_width: int, type="EAN-13") -> str:
+    """Generates text notches (extensions of the side and middle guards making room for optional text.)"""
     side: str = "".join(bit * unit_width for bit in SIDE_GUARD)
     middle: str = "".join(bit * unit_width for bit in MIDDLE_GUARD)
-    empty_space: str = "0" * UNITS_PER_SIDE * unit_width
+    empty_space: str = "0" * UNITS_PER_SIDE[type] * unit_width
     return f"{side}{empty_space}{middle}{empty_space}{side}"
     
 
-def generate_pbm_file(bit_string: str, unit_width: int=1, barcode_height: int=40, notch_height: int=0, border: int=0):
-    """Generates a temporary graphical file with the barcode."""
+def generate_pbm_file(bit_string: str,
+                      type: str="EAN-13",
+                      unit_width: int=1,
+                      barcode_height: int=40,
+                      notch_height: int=0,
+                      border: int=0):
+    """Generates a PBM graphical file with the barcode."""
     width: int = len(bit_string) * unit_width + border * 2
     height: int = barcode_height + notch_height + border * 2
     side_border: str = "0" * border
-    top_and_bottom_border_lines = ("0" * width + "\n") * border
-    barcode_lines = (side_border + "".join(bit * unit_width for bit in bit_string) + side_border + "\n") * barcode_height
+    top_and_bottom_border_lines = ["0" * width + "\n"] * border
+    barcode_lines = [side_border + "".join(bit * unit_width for bit in bit_string) + side_border + "\n"] * barcode_height
     
     with open("barcode.pbm", "w") as output_file:
-        output_file.write(f"P1\n# Barcode\n{width} {height}\n")
+        output_file.write(f"P1\n# {type} BARCODE\n{width} {height}\n")
         output_file.writelines(top_and_bottom_border_lines)
         output_file.writelines(barcode_lines)
         if notch_height:
-            notch_lines = ("".join((side_border, generate_notches(unit_width), side_border, "\n"))) * notch_height
+            notch_lines = ["".join((side_border, generate_notches(unit_width, type), side_border, "\n"))] * notch_height
             output_file.writelines(notch_lines) 
         output_file.writelines(top_and_bottom_border_lines)
 
 
-generate_pbm_file(encode_barcode("9312345678907"), unit_width=6, barcode_height=350, notch_height=35, border=0)
+if __name__ == "__main__":
+    main()
+    
